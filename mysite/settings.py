7922ded9231037098320ed59ee8feda753510186
otajunichiro <otajunitirou@gmail.com>
2021-04-25 12:31:28 +0900

    setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,10 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        # 'USER': 'name',
+        # 'PASSWORD': '',
+        # 'HOST': 'localhost',
+        # 'PORT': '', '
     }
 }
 
